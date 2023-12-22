7d82ca826d493eceb1c7c5ae2d025d9f98f971e9
Leena <109791804+leenahunagund@users.noreply.github.com>
2023-12-23 00:20:53 +0530

    Update longest_palindromic_substring.py

--- longest_palindromic_substring.py
+++ longest_palindromic_substring.py
@@ -14,10 +14,23 @@ Explanation: "aba" is also a valid answer.
 Example 2:
 
 Input: s = "cbbd"
-Output: "bb" 
+Output: "bb" '''
 
---my soln-- 
-62/102 test cases passed '''
+class Solution:
+    def longestPalindrome(self, s):
+        ans=''
+        for i in range(len(s)):
+            ans=max(ans,expand(s,i,i), expand(s,i,i+1), key=len)
+        return ans
+            
+def expand(s,i,j):
+    while i>=0 and j<len(s) and s[i]==s[j]:
+        i-=1
+        j+=1
+    return s[i+1:j]
+    
+'''--my soln-- 
+66/142 test cases passed 
 class Solution(object):
     def longestPalindrome(self, s):
         """
@@ -33,11 +46,25 @@ class Solution(object):
         for i in range(n):
             if s[i]==c[i]:
                 a.append(s[i])
-        
+            elif c[i-1]==c[i]:
+                a.append(c[i])
+            else:
+                if s[i-1]==s[i]:
+                    a.append(s[i])
+            
+        if not a:
+            return s[0]
         k=k.join(a)
         r=r.join(k[::-1])
         if k==r:
             return k
+'''
+
+            
+            
+
+
+
 
 
 
