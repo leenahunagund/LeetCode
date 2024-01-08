45a069ea257404460bbf17c227fb691dca4af0eb
Leena <109791804+leenahunagund@users.noreply.github.com>
2024-01-08 23:44:51 +0530

    Create rangeSumBST.py

---
+++ rangeSumBST.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def rangeSumBST(self, root, low, high):
+        """
+        :type root: TreeNode
+        :type low: int
+        :type high: int
+        :rtype: int
+        """
+        ans=0
+        if root==None: return 0
+        if low<= root.val <=high:
+            ans+=root.val+self.rangeSumBST(root.right,low,high)+self.rangeSumBST(root.left,low,high)
+        if root.val>high:
+            return self.rangeSumBST(root.left,low,high)
+        if root.val<low:
+           return self.rangeSumBST(root.right,low,high)
+        return ans
