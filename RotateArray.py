5daabd6188d7a5cff37322b3c6bc2964ac99bebf
Leena <109791804+leenahunagund@users.noreply.github.com>
2024-02-07 21:38:57 +0530

    Create RotateArray.py

---
+++ RotateArray.py
@@ -0,0 +1,42 @@
+'''189. Rotate Array
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+Example 2:
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]'''
+
+
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+
+      
+        ''' my soln 37/38 testcases passed, except large array
+        for i in range(k):
+            x=nums.pop()
+            nums.insert(0,x)
+        return nums'''
+
+      
+        n = len(nums)
+        k = k % n
+        nums[:] = nums[n-k:] + nums[:n-k]
+
+        
