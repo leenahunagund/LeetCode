8cb11548cae8a727d7fc7da08cf04a570bafc38c
Leena <109791804+leenahunagund@users.noreply.github.com>
2024-03-09 23:17:10 +0530

    Create minCommonVal.py

---
+++ minCommonVal.py
@@ -0,0 +1,58 @@
+'''2540. Minimum Common Value
+Given two integer arrays nums1 and nums2, sorted in non-decreasing order, return the minimum integer common to both arrays.
+If there is no common integer amongst nums1 and nums2, return -1.
+Note that an integer is said to be common to nums1 and nums2 if both arrays have at least one occurrence of that integer.
+
+Example 1:
+Input: nums1 = [1,2,3], nums2 = [2,4]
+Output: 2
+Explanation: The smallest element common to both arrays is 2, so we return 2.
+
+Example 2:
+Input: nums1 = [1,2,3,6], nums2 = [2,3,4,5]
+Output: 2
+Explanation: There are two common elements in the array 2 and 3 out of which 2 is the smallest, so 2 is returned.
+
+
+#my soln, 35/40 cases passed. didnt work for large arrays
+class Solution(object):
+    def getCommon(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: int
+        """
+        x=0
+        for i in nums1:
+            for ele in nums2:
+                if ele in nums1:
+                    x=ele
+                    break
+            break
+        if x:
+            return x
+        else:
+             return -1'''
+class Solution(object):
+    def getCommon(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: int
+        """
+        nums1_dict = {}
+        nums2_dict = {}
+
+        for n in nums1:
+            nums1_dict[n] = True
+        
+        for n in nums2:
+            if n in nums1_dict:
+                return n
+        
+        return -1
+                    
+
+        
+        
+        
