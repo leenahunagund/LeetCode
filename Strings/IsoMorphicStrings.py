003546dd3866c1ae09a8e8d5be7ff197ff2200b6
Leena <109791804+leenahunagund@users.noreply.github.com>
2024-04-02 20:47:56 +0530

    Create IsoMorphicStrings.py

---
+++ Strings/IsoMorphicStrings.py
@@ -0,0 +1,33 @@
+'''205. Isomorphic Strings
+Given two strings s and t, determine if they are isomorphic.
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+Example 1:
+Input: s = "egg", t = "add"
+Output: true
+
+Example 2:
+Input: s = "foo", t = "bar"
+Output: false
+
+Example 3:
+Input: s = "paper", t = "title"
+Output: true
+'''
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        t1=[]
+        t2=[]
+        for ele in s:
+            t1.append(s.index(ele))
+        for ele in t:
+            t2.append(t.index(ele))
+        if t1==t2 :
+            return True
+        return False
